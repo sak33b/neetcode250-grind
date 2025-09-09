8de0e4e7e1335dc8382e1550dae7ef6da07f44c3
sak33b <89086878+sak33b@users.noreply.github.com>
2025-09-09 07:19:09 +0600

    Add solution for Longest Common Subsequence problem

---
+++ 2d-dynamic-programming/1143.longest-common-subsequence.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1143 lang=python3
+#
+# [1143] Longest Common Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp_table = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
+
+        for i in range(1, len(dp_table)):
+            for j in range(1, len(dp_table[0])):
+                if text2[i - 1] == text1[j - 1]:
+                    dp_table[i][j] = 1 + dp_table[i - 1][j - 1]
+                else:
+                    dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1])
+
+        return dp_table[-1][-1]
+
+
+# @lc code=end
